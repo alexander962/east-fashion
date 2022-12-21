00b64260f8782e1397028bc1c6f96699f9b38fc8
Alexander Shamraev <alexander.shamraev@clay.global>
2022-12-21 16:13:17 +0300

    add pages with categories

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,9 +2,18 @@ import React from 'react'
 import cl from 'classnames'
 
 import styles from './index.module.scss'
+import Link from 'next/link';
 const Header = ({className}) => {
   return (
-    <div>Header</div>
+    <div>
+      <div style={{display: 'flex', justifyContent: 'center'}}><Link href={'/'}>East fashion</Link></div>
+      <nav style={{display: 'flex', justifyContent: 'space-between', maxWidth: '400px'}}>
+        <Link href={'/interviews'}>Interviews</Link>
+        <Link href={'/skin-care'}>Skin Care</Link>
+        <Link href={'/culture'}>Culture</Link>
+        <Link href={'/about'}>About us</Link>
+      </nav>
+    </div>
   )
 }
 
