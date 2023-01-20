8b22f2ddf1ebbe201a55c65a47d39bf69c812b2a
Alexander Shamraev <alexander.shamraev@clay.global>
2023-01-20 10:39:58 +0300

    add 404 page

---
+++ src/pages/404/index.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import Head from 'next/head';
+import cl from 'classnames'
+import styles from './index.module.scss';
+import { Footer, Header, Section } from '@/components';
+
+const PageNotFound = () => {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [posts, setPosts] = useState([]);
+  const [totalPosts, setTotalPosts] = useState(0);
+  const [visiblePopularsPosts, setVisiblePopularsPosts] = useState(true);
+
+  return (
+    <Section>
+      <Head>
+        <title>Page Not Found</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header searchVisible={false} setModalVisible={setModalVisible} setPosts={setPosts} setVisiblePopularsPosts={setVisiblePopularsPosts} setTotalPosts={setTotalPosts} />
+      <div className={cl(styles.notFound)}>
+        <div>
+          <h3>Oops:(</h3>
+          <h1>404</h1>
+          <h6>page not found</h6>
+        </div>
+      </div>
+      <Footer />
+    </Section>
+  )
+};
+
+export default PageNotFound;
