81a8dcc5873fe519736955491d6e526a88e4605b
aleksandrsamraev <alex.shamrayev@gmail.com>
2023-03-02 15:37:31 +0300

    add Google Analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,23 @@
 import '../styles/reset.scss';
 import '../styles/global.scss';
-import '../styles/slider.scss'
+import '../styles/slider.scss';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-8207NCWT7P" />;
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-8207NCWT7P');
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
