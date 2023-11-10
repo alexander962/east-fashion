c271479b06701e18bf61220f0c947c3eee648381
aleksandrsamraev <alex.shamrayev@gmail.com>
2023-11-10 14:55:02 +0300

    fix bug Posts

--- src/components/Posts/index.jsx
+++ src/components/Posts/index.jsx
@@ -1,35 +1,8 @@
-// import React from 'react';
-// import cl from 'classnames';
-//
-// import { Post } from '@/components';
-// import styles from './index.module.scss';
-// import Sidebar from '@/components/Sidebar';
-//
-// const Posts = ({ posts }) => {
-//   return (
-//     <div className={cl(styles.posts)}>
-//       {posts.map((post, index) => {
-//         let classVariant = post.additionalImage ? 3 : index % 2 ? 1 : 2;
-//
-//         return (
-//           <div key={post?._id} className={cl(styles.postWrapper)}>
-//             <Post post={post} classVariant={classVariant} index={index} />
-//             <hr className={styles.postHr} />
-//           </div>
-//         );
-//       })}
-//     </div>
-//   );
-// };
-//
-// export default Posts;
-
 import React from 'react';
 import cl from 'classnames';
 
 import { Post } from '@/components';
 import styles from './index.module.scss';
-import Sidebar from '@/components/Sidebar';
 
 const Posts = ({ posts }) => {
   return (
