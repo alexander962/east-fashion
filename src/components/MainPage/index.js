28f4e69ce6c03b8ef52e08e6308094b2b19620ce
aleksandrsamraev <alex.shamrayev@gmail.com>
2023-11-10 14:44:33 +0300

    fix bug pages/index

--- src/components/MainPage/index.js
+++ src/components/MainPage/index.js
@@ -1,6 +1,90 @@
-import React, { useState } from 'react';
-import cl from 'classnames';
+// import React, { useState } from 'react';
+// import cl from 'classnames';
+//
+// import { Button, Posts } from '@/components';
+// import PopularPosts from '@/components/PopularPosts';
+// import FavouritesPosts from '@/components/FavouritesPosts';
+// import styles from './index.module.scss';
+// import Sidebar from '@/components/Sidebar';
+// import SidebarMobile from '@/components/SideBarMobile';
+// import useWindowSize from '@/hooks/useWindowSize';
+//
+// const LOAD_MORE_STEP = 4;
+// const MainPage = ({
+//   posts,
+//   setPosts,
+//   total,
+//   favouritesPosts,
+//   popularPosts,
+//   sideBarPosts,
+//   visiblePopularsPosts,
+//   visibleSearchResult,
+// }) => {
+//   const [loading, setLoading] = useState(false);
+//   const [loadedAmount, setLoadedAmount] = useState(LOAD_MORE_STEP);
+//   const { width } = useWindowSize();
+//
+//   const isLoadButton = total > loadedAmount;
+//   const getMorePosts = async () => {
+//     setLoading(true);
+//
+//     try {
+//       const data = await fetch(`/api/posts?start=${loadedAmount}&end=${loadedAmount + LOAD_MORE_STEP}`).then(response =>
+//         response.json()
+//       );
+//       setLoadedAmount(loadedAmount + LOAD_MORE_STEP);
+//       setPosts([...posts, ...data.posts]);
+//
+//       setLoading(false);
+//     } catch (error) {
+//       console.log(error);
+//       setLoading(false);
+//     }
+//   };
+//
+//   return (
+//     <main className={cl(styles.mainPage)}>
+//       {visiblePopularsPosts && (
+//         <>
+//           <PopularPosts posts={popularPosts} />
+//           <FavouritesPosts posts={favouritesPosts} />
+//           {width <= 768 && <SidebarMobile sideBarPosts={sideBarPosts} />}
+//         </>
+//       )}
+//       {posts.length === 0 ? (
+//         <div className={cl(styles.mainPageNotFound)}>Aucun article trouvé</div>
+//       ) : (
+//         <>
+//           <div className={styles.mainPageBlock}>
+//             <div className={cl(styles.mainPage__header)}>
+//               {/*<hr className={styles.mainPageHrMain} />*/}
+//               {!visibleSearchResult ? (
+//                 <span className={cl(styles.mainPage__headerText)}>Dernières publications</span>
+//               ) : (
+//                 <span className={cl(styles.mainPage__headerText)}>Dernières publications</span>
+//               )}
+//               <Posts posts={posts} />
+//             </div>
+//             <Sidebar sideBarPosts={sideBarPosts} />
+//           </div>
+//         </>
+//       )}
+//
+//       {isLoadButton && (
+//         <div className={cl(styles.mainPageBtn)}>
+//           <Button onClick={getMorePosts} disabled={loading} className={cl(styles.mainPage__button)}>
+//             VOIR PLUS
+//           </Button>
+//         </div>
+//       )}
+//     </main>
+//   );
+// };
 
+// export default MainPage;
+
+import React, { useState, useCallback } from 'react';
+import cl from 'classnames';
 import { Button, Posts } from '@/components';
 import PopularPosts from '@/components/PopularPosts';
 import FavouritesPosts from '@/components/FavouritesPosts';
@@ -10,6 +94,7 @@ import SidebarMobile from '@/components/SideBarMobile';
 import useWindowSize from '@/hooks/useWindowSize';
 
 const LOAD_MORE_STEP = 4;
+
 const MainPage = ({
   posts,
   setPosts,
@@ -25,22 +110,25 @@ const MainPage = ({
   const { width } = useWindowSize();
 
   const isLoadButton = total > loadedAmount;
-  const getMorePosts = async () => {
+
+  const getMorePosts = useCallback(async () => {
+    if (loading) return;
+
     setLoading(true);
 
     try {
-      const data = await fetch(`/api/posts?start=${loadedAmount}&end=${loadedAmount + LOAD_MORE_STEP}`).then(response =>
-        response.json()
-      );
+      const response = await fetch(`/api/posts?start=${loadedAmount}&end=${loadedAmount + LOAD_MORE_STEP}`);
+      const data = await response.json();
+
       setLoadedAmount(loadedAmount + LOAD_MORE_STEP);
-      setPosts([...posts, ...data.posts]);
+      setPosts(prevPosts => [...prevPosts, ...data.posts]);
 
       setLoading(false);
     } catch (error) {
-      console.log(error);
+      console.error('Error fetching more posts:', error);
       setLoading(false);
     }
-  };
+  }, [loading, loadedAmount, setPosts]);
 
   return (
     <main className={cl(styles.mainPage)}>
@@ -54,20 +142,13 @@ const MainPage = ({
       {posts.length === 0 ? (
         <div className={cl(styles.mainPageNotFound)}>Aucun article trouvé</div>
       ) : (
-        <>
-          <div className={styles.mainPageBlock}>
-            <div className={cl(styles.mainPage__header)}>
-              {/*<hr className={styles.mainPageHrMain} />*/}
-              {!visibleSearchResult ? (
-                <span className={cl(styles.mainPage__headerText)}>Dernières publications</span>
-              ) : (
-                <span className={cl(styles.mainPage__headerText)}>Dernières publications</span>
-              )}
-              <Posts posts={posts} />
-            </div>
-            <Sidebar sideBarPosts={sideBarPosts} />
+        <div className={styles.mainPageBlock}>
+          <div className={cl(styles.mainPage__header)}>
+            <span className={cl(styles.mainPage__headerText)}>Dernières publications</span>
+            <Posts posts={posts} />
           </div>
-        </>
+          <Sidebar sideBarPosts={sideBarPosts} />
+        </div>
       )}
 
       {isLoadButton && (
